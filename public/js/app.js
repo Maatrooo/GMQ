105b861127072d4fc8c864d4a0e7d4b2fb3e956e
Maatrooo <matheobernard59@gmail.com>
2024-03-13 12:07:57 +0100

    Locker for MultiRoom

--- public/js/app.js
+++ public/js/app.js
@@ -3,32 +3,71 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let isRoomCreator = false; // Variable pour savoir si l'utilisateur est le créateur de la salle
 
-  
+  // Gestionnaire d'événement lorsque la case à cocher est modifiée
+  const passwordCheckbox = document.querySelector('#password-checkbox');
+  passwordCheckbox.addEventListener('change', () => {
+    const passwordInputContainer = document.querySelector('#password-input-container');
+    passwordInputContainer.style.display = passwordCheckbox.checked ? 'block' : 'none';
+  });
+
 
   function createRoom() {
     const roomNameInput = document.querySelector('#room-name-input');
     const roomName = roomNameInput.value.trim();
+    const hasPassword = document.querySelector('#password-checkbox').checked;
+    console.log(hasPassword);
+    let password = '';
+  
+    if (hasPassword) {
+      const passwordInput = document.querySelector('#password-input');
+      password = passwordInput.value.trim();
+    }
   
     if (roomName) {
-      socket.emit('createRoom', roomName, (roomId) => {
-        // Redirigez l'utilisateur vers la salle nouvellement créée en utilisant son identifiant
+      socket.emit('createRoom', roomName, hasPassword, password, (roomId) => {
         window.location.href = `/room/${roomId}`;
-        isRoomCreator = true; // L'utilisateur est le créateur de la salle
+        isRoomCreator = true;
       });
-      roomNameInput.value = ''; // Effacez le champ de saisie après la création
+      roomNameInput.value = '';
     } else {
       alert('Veuillez entrer un nom de salle valide.');
     }
   }
+  
 
 
 
-function joinRoom(roomId) {
-  console.log(roomId);
-  // Par exemple, rediriger l'utilisateur vers la salle spécifique
-  window.location.href = `/room/${roomId}`;
-  isRoomCreator = false;
-}
+  function joinRoom(roomId) {
+    console.log(roomId);
+    // Par exemple, rediriger l'utilisateur vers la salle spécifique
+      window.location.href = `/room/${roomId}`;
+    isRoomCreator = false;
+  }
+  
+  function connexion(roomId){
+    socket.emit('hasPassword', { roomId });
+    socket.on("passwordTrue", () => {
+      const password = prompt('Veuillez entrer le mot de passe de la salle :');
+      socket.emit('connexion', { roomId , password});
+      // Écouter les réponses du serveur
+      socket.on('incorrectPassword', () => {
+        alert('Mot de passe incorrect. Veuillez réessayer.');
+      });
+    
+      socket.on('roomNotFound', () => {
+        alert("La salle n'existe pas . Veuillez réessayer.");
+      });
+    
+      socket.on('goodPassword', () => {
+        joinRoom(roomId);
+      });
+    });
+
+    socket.on("passwordFalse", () => {    
+      joinRoom(roomId);
+    });
+  }
+  
   
   // Écoutez l'événement "listRooms" pour afficher la liste des salles
   socket.on('listRooms', (rooms) => {
@@ -38,12 +77,22 @@ function joinRoom(roomId) {
     rooms.forEach((room) => {
       const listItem = document.createElement('li');
       const button = document.createElement('button');
-      button.textContent = room.name;
-      button.addEventListener('click', () => {
-        joinRoom(room.id);
-      });
-      listItem.appendChild(button);
-      roomsList.appendChild(listItem);    
+      if(room.hasPassword == false){
+        button.innerHTML = room.name +"<i class='fa fa-unlock'></i>";
+        button.addEventListener('click', () => {
+          connexion(room.id);
+        });
+        listItem.appendChild(button);
+        roomsList.appendChild(listItem);
+      }
+      else{
+        button.innerHTML = room.name +"<i class='fa fa-lock'></i>";
+        button.addEventListener('click', () => {
+          connexion(room.id);
+        });
+        listItem.appendChild(button);
+        roomsList.appendChild(listItem);
+      }    
     });
   });
   
